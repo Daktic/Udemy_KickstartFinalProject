d8ff9ccb3f1ae2c6a73671aafac185e9ac9ee5f3
Daktic <58493066+Daktic@users.noreply.github.com>
2022-01-17 20:09:16 -0500

    test
    test

---
+++ kickstart/pages/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Card, Button } from 'semantic-ui-react';
+import factory from '../ethereum/factory';
+import Layout from '../components/Layouts';
+import { Link } from '../routes';
+
+
+class CampaignIndex extends Component {
+    static async getInitialProps() {
+        const campaigns = await factory.methods.getDeployedCampaigns().call();
+
+        return { campaigns };
+    }
+    rendercampaigns() {
+        const items = this.props.campaigns.map(address => {
+            return {
+                header: address,
+                description: (
+                <Link route={`/campaigns/${address}`}>
+                    <a>View Campaign</a>
+                </Link>
+                ),
+                fluid: true
+            };
+        });
+
+        return <Card.Group items={items} />;
+    }
+
+    render() {
+        return (
+        <Layout>
+            <div>
+            <h3>Open Campaigns</h3>
+
+            <Link route='/campaigns/new'>
+                <a>
+                    <Button 
+                    content='Create Campaign'
+                    icon='add'
+                    primary
+                    floated='right'
+                    />
+                </a>
+            </Link>
+
+            {this.rendercampaigns()}
+            </div>
+        </Layout>
+        );
+    }
+} 
+
+export default CampaignIndex;
